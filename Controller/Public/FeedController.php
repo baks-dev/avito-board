b14f789becbb9e787ab68bb58af1ea0386aad6c0
adminap <shestopalov92@gmail.com>
2024-09-05 22:58:21 +0300

    master:fix

--- Controller/Public/FeedController.php
+++ Controller/Public/FeedController.php
@@ -3,32 +3,44 @@
 namespace BaksDev\Avito\Board\Controller\Public;
 
 use BaksDev\Avito\Board\Repository\AllProductsWithMapper\AllProductsWithMapperInterface;
+use BaksDev\Core\Cache\AppCacheInterface;
 use BaksDev\Core\Controller\AbstractController;
 use BaksDev\Core\Type\UidType\ParamConverter;
 use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\ItemInterface;
 
 #[AsController]
 final class FeedController extends AbstractController
 {
     #[Route('/avito-board/{profile}/feed.xml', name: 'public.export.feed', methods: ['GET'])]
     public function feed(
+        AppCacheInterface $cache,
         AllProductsWithMapperInterface $allProductsWithMapping,
         #[ParamConverter(UserProfileUid::class)] $profile,
     ): Response {
 
         $products = $allProductsWithMapping->findAll($profile);
 
-        $response = $this->render(
-            [
-                'products' => $products,
-                'profile' => (string)$profile,
-            ],
-            file: 'export.html.twig'
-        );
+        $cache = $cache->init('avito-board');
 
+        $feed = $cache->get('feed-' . $profile, function (ItemInterface $item) use ($products): string {
+
+            $item->expiresAfter(300);
+
+            return $this->render(
+                [
+                    'products' => $products,
+                ],
+                file: 'export.html.twig'
+            )->getContent();
+        });
+
+        $response = new Response();
+
+        $response->setContent($feed);
         $response->headers->set('Content-Type', 'application/xml');
 
         return $response;
